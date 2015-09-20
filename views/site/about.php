59250bd65dff6511d275c68378b4f66cf6a3870e
hajdar dushku <hajdardushku@gmail.com>
2015-09-20 14:13:53 +0200

    Yii2 Fine Uploader and update to yii2 rrssb
    harrytang/yii2-fineuploader

--- views/site/about.php
+++ views/site/about.php
@@ -39,15 +39,77 @@ echo SideNav::widget([
 ]);
 ?>
 
-<?= \imanilchaudhari\rrssb\ShareBar::widget([
+<?=\imanilchaudhari\rrssb\ShareBar::widget([
         'title' => 'Title Content', // i.e. $model->title
-        'url' => Yii::$app->request->absoluteUrl, 
+        'media' => 'image.jpg', // Media Content
         'networks' => [
-            'Email' => [],
-            'Facebook' => [],
-            'Twitter' => [],
-            'GooglePlus' => [],
-            'LinkedIn' => []
-            ]
+            'Email',
+            'Facebook',
+            'Github',
+            'GooglePlus',
+            'Hackernews',
+            'LinkedIn',
+            'Pinterest',
+            'Pocket',
+            'Reddit',
+            'Tumblr',
+            'Twitter',
+            'Vk',
+            'Youtube'  
+        ]
     ]); 
 ?>
+
+<div class="uploader"></div>
+<?= harrytang\fineuploader\Fineuploader::widget([
+        'options' => [
+            'request' => [
+                'endpoint' => Yii::$app->urlManager->createUrl(['/your-handler']),
+                'params' => [Yii::$app->request->csrfParam => Yii::$app->request->csrfToken]
+            ],
+            'validation' => [
+                'allowedExtensions' => ['jpeg', 'jpg', 'png', 'bmp', 'gif'],
+            ],
+            'classes' => [
+                'success' => 'alert alert-success hidden',
+                'fail' => 'alert alert-error'
+            ],
+            //'multiple'=>false,
+        ],
+        //'events' => [
+        //    'allComplete' => '$("#loading").modal("hide"); ',
+        //]
+    ])
+?>
+
+<?php
+// first lets setup the center of our map
+$center = new dosamigos\leaflet\types\LatLng(['lat' => 51.508, 'lng' => -0.11]);
+
+// now lets create a marker that we are going to place on our map
+$marker = new \dosamigos\leaflet\layers\Marker(['latLng' => $center, 'popupContent' => 'Hi!']);
+
+// The Tile Layer (very important)
+$tileLayer = new \dosamigos\leaflet\layers\TileLayer([
+   'urlTemplate' => 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg',
+    'clientOptions' => [
+        'attribution' => 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> ' .
+        '<img src="http://developer.mapquest.com/content/osm/mq_logo.png">, ' .
+        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        'subdomains' => '1234'
+    ]
+]);
+
+// now our component and we are going to configure it
+$leaflet = new \dosamigos\leaflet\LeafLet([
+    'center' => $center, // set the center
+]);
+// Different layers can be added to our map using the `addLayer` function.
+$leaflet->addLayer($marker)      // add the marker
+        ->addLayer($tileLayer);  // add the tile layer
+
+// finally render the widget
+echo \dosamigos\leaflet\widgets\Map::widget(['leafLet' => $leaflet]);
+
+// we could also do
+// echo $leaflet->widget();
